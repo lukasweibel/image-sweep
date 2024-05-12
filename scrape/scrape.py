08d4a42e18f346952c51f181480868c653068904
Lukas Weibel <lukasweibel@air-von-lukas.zhaw.ch>
2024-05-12 11:46:02 +0200

    Extend scraper

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -7,20 +7,15 @@ from pathlib import Path
 import os
 import glob
 
-def start_scraping():
-    timeout = 3000
+def start_scraping_screenshots(device):
+    timeout = 4000
     
-    #delete_png_files('data/screenshots')
-    
-    mobile_device = 'iPhone X'
-    
-    devices = ['iPhone X', 'iPhone 13 mini', 'Samsung Galaxy S21']
+    mobile_device = device
     
     with open('scrape/websites.txt', 'r') as file:
         websites = [line.strip() for line in file.readlines()]
 
     def run(playwright):
-        failures = 0
         browser = playwright.chromium.launch(headless=True)
         device = playwright.devices[mobile_device]
         context = browser.new_context(**device)
@@ -52,10 +47,65 @@ def start_scraping():
 
         context.close()
         browser.close()
-        print("fauilures: " + str(failures))
         
     with sync_playwright() as playwright:
         run(playwright)
+        
+def start_scraping_whatsapp():
+    timeout = 3000
+    
+    download_path = 'data/screenshots'
+    
+    mobile_device = 'iPhone X'
+
+    def run(playwright):
+        failures = 0
+        browser = playwright.chromium.launch(headless=True)
+        device = playwright.devices[mobile_device]
+        #context = browser.new_context(**device)
+        context = browser.new_context(
+            **device,
+            accept_downloads=True
+        )
+        
+        try:
+            page = context.new_page()
+            page.goto("https://prankshit.com/fake-whatsapp-chat-generator.php", timeout=timeout)
+            
+            page.wait_for_load_state('load')
+            
+            try:
+                button = page.locator('text="Download image "')
+                if button.is_visible():
+                    with page.expect_download() as download_info:
+                        button.click()
+                    download = download_info.value
+
+                    original_path = download.path()
+                    
+                    unique_id = uuid.uuid4()  # Generate a unique identifier
+                    file_name = f"{unique_id}.png"  # Use UUID to form a new file name
+                    
+                    new_path = os.path.join(download_path, file_name)
+                    os.rename(original_path, new_path)
+                    print(f"Downloaded file saved to: {new_path}")
+            except Exception as e:
+                print(f"Failed to interact, error: {e}")
+        except PlaywrightTimeoutError as e:
+                print(f"Encountered a timeout: {e}")
+
+        context.close()
+        browser.close()
+        
+    with sync_playwright() as playwright:
+        run(playwright)
+        
+def run_scraping():
+    delete_png_files('data/screenshots')
+    start_scraping_screenshots('Desktop Chrome')
+    start_scraping_screenshots('iPhone X')
+    for _ in range(15):
+        start_scraping_whatsapp()
     
 def delete_png_files(folder_path):
     file_pattern = os.path.join(folder_path, '*.png')
@@ -66,4 +116,4 @@ def delete_png_files(folder_path):
         os.remove(file_path)
 
 if __name__ == "__main__":
-    start_scraping()
+    run_scraping()
